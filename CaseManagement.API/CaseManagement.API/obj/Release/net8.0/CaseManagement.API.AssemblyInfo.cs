01c38f6e3cd5062571fcdb6dbc5ec3b20a1065fa
Srikant-Vel <srikant.peddy@veltris.com>
2024-12-11 11:46:30 +0530

    Initial changes of Business service - signup

--- CaseManagement.API/CaseManagement.API/obj/Release/net8.0/CaseManagement.API.AssemblyInfo.cs
+++ CaseManagement.API/CaseManagement.API/obj/Release/net8.0/CaseManagement.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaseManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09090313f18028f5ca75e0e61ba8d439e1f1de4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8287e6726fad13104c1b8e1b1b9434adb772438")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaseManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaseManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CaseManagement.Business/Services/SignupService.cs
@@ -0,0 +1,42 @@
+﻿using CaseManagement.Business.Models;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseManagement.Business.Services
+{
+
+    public class SignupService
+    {
+        private readonly IValidator<SignupModel> _validator;
+
+        public SignupService(IValidator<SignupModel> validator)
+        {
+            _validator = validator;
+        }
+
+
+        public List<string> ValidateSignUpDetails(SignupModel userDataModel)
+        {            
+            List<string> validationErrors = new List<string>();
+
+            ValidationResult validation = _validator.Validate(userDataModel);
+
+            if (!(validation.IsValid))
+            {
+
+                foreach (var error in validation.Errors) {
+                    validationErrors.Add(error.ErrorMessage); 
+                }
+                
+            }
+            return validationErrors;
+
+        }
+    }
+}
