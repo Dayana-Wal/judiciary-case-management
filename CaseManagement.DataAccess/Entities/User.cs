0c8116825379065d410cc7dd618c4ac4786eabf4
Veerendra-wal <veerendra.p@veltris.com>
2024-12-11 11:16:28 +0530

    Added initial migration and entities

---
+++ CaseManagement.DataAccess/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CaseManagement.DataAccess.Entities;
+
+public partial class User
+{
+    public Guid Id { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string PasswordSalt { get; set; } = null!;
+
+    public int RoleId { get; set; }
+
+    public Guid PersonId { get; set; }
+
+    public virtual Person Person { get; set; } = null!;
+
+    public virtual LookupConstant Role { get; set; } = null!;
+}
--- CaseManagement.DataAccess/Migrations/InitialMigration.cs
+++ CaseManagement.DataAccess/Migrations/InitialMigration.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentMigrator;
 
 namespace CaseManagement.DataAccess.Migrations
 {
-    internal class InitialMigration
+    [Migration(202412110938)]
+    public class CreateInitialSchema : Migration
     {
+        public override void Up()
+        {
+            Create.Table("LookupConstant")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Code").AsString(50).NotNullable()
+                .WithColumn("Text").AsString(255).NotNullable()
+                .WithColumn("Type").AsString(50).NotNullable();
+
+            Create.Table("Person")
+                .WithColumn("Id").AsGuid().PrimaryKey()
+                .WithColumn("Name").AsString(255).NotNullable()
+                .WithColumn("Email").AsString(255).NotNullable().Unique()
+                .WithColumn("Contact").AsInt64().NotNullable()
+                .WithColumn("DateOfBirth").AsDateTime().NotNullable()
+                .WithColumn("Gender").AsString(50).NotNullable();
+
+            Create.Table("User")
+                .WithColumn("Id").AsGuid().PrimaryKey()
+                .WithColumn("UserName").AsString(255).NotNullable().Unique()
+                .WithColumn("PasswordHash").AsString(255).NotNullable()
+                .WithColumn("PasswordSalt").AsString(255).NotNullable()
+                .WithColumn("RoleId").AsInt32().NotNullable().ForeignKey("LookupConstant", "Id")
+                .WithColumn("PersonId").AsGuid().NotNullable().ForeignKey("Person", "Id");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("User");
+            Delete.Table("Person");
+            Delete.Table("LookupConstant");
+        }
     }
 }
