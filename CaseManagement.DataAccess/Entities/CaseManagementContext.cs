e1cfc151958db73207f02fe5e3fb9b12bd35452c
wal-anudeepthi <anudeepthi.k@veltris.com>
2024-12-19 18:15:57 +0530

    Added jquery file

--- CaseManagement.DataAccess/Entities/CaseManagementContext.cs
+++ CaseManagement.DataAccess/Entities/CaseManagementContext.cs
@@ -35,10 +35,6 @@ public partial class CaseManagementContext : DbContext
 
     public virtual DbSet<VersionInfo> VersionInfos { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-H63SR17\\SQLEXPRESS01;Initial Catalog=CaseManagement;Integrated Security=True;TrustServerCertificate=true");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Case>(entity =>
