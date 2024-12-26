4547f3f39e9b57490ec618ffc25788a5e6ca34d2
wal-anudeepthi <anudeepthi.k@veltris.com>
2024-12-26 10:55:55 +0530

    For testing

---
+++ CaseManagement.API/CaseManagement.API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using CaseManagement.API.Filters;
+
+namespace CaseManagement.API.Controllers
+{
+    public class TestController : BaseController
+    {
+        [CustomAuthorization("Admin")]
+        [HttpGet("token")]
+        public IActionResult Index()
+        {
+            return Content("from test");
+        }
+    }
+}
