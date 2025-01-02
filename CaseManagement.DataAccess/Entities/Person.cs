1275042299a8924f7aebc899300f4364c9adbeac
Dayana-Wal <dayana.b@veltris.com>
2025-01-02 15:02:13 +0530

    Added Collection File

--- CaseManagement.DataAccess/Entities/Person.cs
+++ CaseManagement.DataAccess/Entities/Person.cs
@@ -27,6 +27,8 @@ public partial class Person
     [JsonIgnore]
     public virtual ICollection<Case> CaseVictims { get; set; } = new List<Case>();
 
+    public virtual ICollection<File> Files { get; set; } = new List<File>();
+
     public virtual ICollection<Otp> Otps { get; set; } = new List<Otp>();
 
     public virtual ICollection<Request> Requests { get; set; } = new List<Request>();
