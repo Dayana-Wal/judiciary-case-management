27e9e9269cbfd2a69e5c394308239a93a11de086
Veerendra-wal <veerendra.p@veltris.com>
2025-01-02 15:50:39 +0530

    dynamic role id

---
+++ CaseManagement.Business/Providers/RoleIdProvider.cs
@@ -0,0 +1,21 @@
+﻿using CaseManagement.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaseManagement.Business.Providers
+{
+    public class RoleIdProvider
+    {
+        private readonly CaseManagementContext _context;
+
+        public RoleIdProvider(CaseManagementContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> GetRoleId(string code, string type)
+        {
+            var lookupConstantdata = await _context.LookupConstants.FirstOrDefaultAsync(c => c.Type == type && c.Code == code);
+            return lookupConstantdata?.Id ?? 0;
+            
+        }
+    }
+}
