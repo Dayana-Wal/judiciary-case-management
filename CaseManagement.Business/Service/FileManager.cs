ceb5b73f03f1e874fc8465ce24045786e2e4bd13
wal-anudeepthi <anudeepthi.k@veltris.com>
2025-01-07 12:04:39 +0530

    Creating directory

--- CaseManagement.Business/Service/FileManager.cs
+++ CaseManagement.Business/Service/FileManager.cs
@@ -15,34 +15,30 @@ namespace CaseManagement.Business.Service
         }
         public async Task<OperationResult<List<string>>> UploadFile(FilesCommand filesCommand)
         {
-            var uploadsPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
-            if (!Directory.Exists(uploadsPath))
-            {
+                var uploadsPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
                 Directory.CreateDirectory(uploadsPath);
-            }
-            List<Files> filesToInsert = new List<Files>();
+                List<Files> filesToInsert = new List<Files>();
 
-            foreach (var file in filesCommand.Files)
-            {
-                var currentTimestamp = DateTime.Now.ToString("yyyyMMdd_HHmmssfff");
-                //TODO: Doubt about Generating file name with current timestamp
-                var fileName = $"{Path.GetFileNameWithoutExtension(file.FileName)}_{currentTimestamp}{Path.GetExtension(file.FileName)}";
-                var filePath = Path.Combine(uploadsPath, fileName);
-                using (var stream = new FileStream(filePath, FileMode.Create))
+                foreach (var file in filesCommand.Files)
                 {
-                    await file.CopyToAsync(stream);
+                    var currentTimestamp = DateTime.Now.ToString("yyyyMMdd_HHmmssfff");
+                    var fileName = $"{Path.GetFileNameWithoutExtension(file.FileName)}_{currentTimestamp}{Path.GetExtension(file.FileName)}";
+                    var filePath = Path.Combine(uploadsPath, fileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    filesToInsert.Add(new Files
+                    {
+                        Id = NewUlid(),
+                        FileName = fileName,
+                        FilePath = Path.Combine("wwwroot","uploads" , fileName),
+                        FileTypeId = filesCommand.FileTypeId,
+                        UploadedBy = filesCommand.UploadedBy,
+                    });
                 }
-                filesToInsert.Add(new Files
-                {
-                    Id = NewUlid(),
-                    FileName = fileName,
-                    FilePath = Path.Combine("wwwroot","uploads" , fileName),
-                    FileTypeId = filesCommand.FileTypeId,
-                    UploadedBy = filesCommand.UploadedBy,
-                });
-            }
-            var res = await _fileCommandHandler.AddFilesAsync(filesToInsert);
-            return res;
+                var res = await _fileCommandHandler.AddFilesAsync(filesToInsert);
+                return res;
         }
     }
 }
