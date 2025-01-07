fd54a038efdb45b982b775d48949875ceba0c49a
Dayana-Wal <dayana.b@veltris.com>
2025-01-07 13:49:46 +0530

    Handled logout functionality

---
+++ CaseManagement.Web/CaseManagement.Web/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaseManagement.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public BaseController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected string GetApiBaseUrl()
+        {
+            return _configuration["ApiSettings:BaseUrl"];
+        }
+    }
+}
---
+++ CaseManagement.Web/CaseManagement.Web/wwwroot/js/pages/logout.js
@@ -0,0 +1,50 @@
+﻿function logout() {
+    // Clear session-related data
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('user');
+
+    // Show a confirmation message
+    alert('You have been logged out successfully.');
+
+    // Redirect to the login page
+    window.location.href = '/User/Login';
+}
+
+function renderNavLinks() {
+    const authToken = localStorage.getItem("authToken");
+    const user = localStorage.getItem("user");
+    const navLinks = document.getElementById("navLinks");
+
+    if (authToken && user) {
+        navLinks.innerHTML = `
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="/CaseSearch/Search">Search</a>
+            </li>
+            <li class="nav-item ms-auto">
+                <a class="nav-link text-dark" href="#" onclick="logout()">Logout</a>
+            </li>
+        `;
+    } else {
+        navLinks.innerHTML = `
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="/User/Signup">Signup</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-dark" href="#" id="loginDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Login
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="loginDropdown">
+                    <li>
+                        <a class="dropdown-item" href="/User/Login?role=user">Login</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="/User/Login?role=admin">Login as Admin</a>
+                    </li>
+                </ul>
+            </li>
+        `;
+    }
+}
+
+// Initialize navigation links
+renderNavLinks();
