22decdc1cd6f20e3276fa9c0f94fe40c43353955
ZaheerShaik-SDE <shaikzaheer9139@gmail.com>
2025-01-10 14:16:24 +0530

    cache - updated

--- CaseManagement.Business/Utility/CacheUtility.cs
+++ CaseManagement.Business/Utility/CacheUtility.cs
@@ -1,47 +1,4 @@
-﻿//using CaseManagement.DataAccess.Entities;
-//using Microsoft.Extensions.Caching.Memory;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace CaseManagement.Business.Utility
-//{
-//    public class CacheUtility
-//    {
-//        private readonly CaseManagementContext _caseManagementContext;
-//        private readonly IMemoryCache _memoryCache;
-
-//        public CacheUtility(CaseManagementContext caseManagementContext , IMemoryCache memoryCache)
-//        {
-//            _caseManagementContext = caseManagementContext;
-//            _memoryCache = memoryCache;
-//        }
-
-//        public List<string> GetCaseTypes()
-//        {
-//            const string cacheKey = "CaseTypes";
-
-//            if (_memoryCache.TryGetValue(cacheKey, out List<string> CaseTypes))
-//            {
-//                CaseTypes = _caseManagementContext.LookupConstants
-//                    .Select(c => c.Text)
-//                    .ToList();
-
-//                _memoryCache.Set(cacheKey, CaseTypes);
-
-//            }
-
-
-//            return CaseTypes;
-//        }
-
-
-//    }
-//}
-
-using CaseManagement.DataAccess.Entities;
+﻿using CaseManagement.DataAccess.Entities;
 using System.Runtime.Caching;
 
 namespace CaseManagement.Business.Utility
