f384ef4fba9ea52bab35a593584c64906ecdad25
wal-anudeepthi <anudeepthi.k@veltris.com>
2025-01-10 13:03:33 +0530

    Reverted context changes

--- CaseManagement.DataAccess/Entities/CaseManagementContext.cs
+++ CaseManagement.DataAccess/Entities/CaseManagementContext.cs
@@ -37,7 +37,7 @@ public partial class CaseManagementContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=WL-MS-719\\SQLEXPRESS;Initial Catalog=CaseManagement;Integrated Security=True;TrustServerCertificate=true;");
+        => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=CaseManagement;Integrated Security=True;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
