4dea010bcd946ac603fdc3427dae56c8c74180cb
Dayana-Wal <dayana.b@veltris.com>
2025-01-23 12:30:55 +0530

    Added dateofincident migration

---
+++ CaseManagement.DataAccess/Migrations/DateOfIncidentMigration.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseManagement.DataAccess.Migrations
+{
+    [Migration(202501231133)]
+    public class DateOfIncidentMigration : Migration
+    {
+        public override void Down()
+        {
+            Delete.Column("DateOfIncident").FromTable("case");
+        }
+
+        public override void Up()
+        {
+            Alter.Table("case")
+                 .AddColumn("DateOfIncident")
+                 .AsDateTime()
+                 .Nullable();
+
+        }
+    }
+}
