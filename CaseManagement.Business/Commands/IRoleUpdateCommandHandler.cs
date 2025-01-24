aa57dad2eaacb09e5f17273c49ea616a695b49d5
Dayana-Wal <dayana.b@veltris.com>
2025-01-24 16:58:54 +0530

    Api for editing the user role

---
+++ CaseManagement.Business/Commands/IRoleUpdateCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using CaseManagement.Business.Common;
+using CaseManagement.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseManagement.Business.Commands
+{
+    public interface IRoleUpdateCommandHandler
+    {
+        Task<User?> GetUserByIdAsync(string userId);
+        Task<OperationResult> UpdateUserAsync(User user);
+    }
+}
---
+++ CaseManagement.Business/Commands/RoleUpdateCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using CaseManagement.Business.Common;
+using CaseManagement.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CaseManagement.Business.Commands
+{
+    public class RoleUpdateCommandHandler : IRoleUpdateCommandHandler
+    {
+        private readonly CaseManagementContext _context;
+
+        public RoleUpdateCommandHandler(CaseManagementContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserByIdAsync(string userId)
+        {
+            return await _context.Users
+                .Include(u => u.Role) 
+                .FirstOrDefaultAsync(u => u.Id == userId);  
+        }
+
+        public async Task<OperationResult> UpdateUserAsync(User user)
+        {
+
+            if (user == null)
+            {
+                return OperationResult.Failed(message: "User cannot be null.");
+            }
+
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+
+            return OperationResult.Success("User updated successfully.");
+            
+        }
+    }
+}
