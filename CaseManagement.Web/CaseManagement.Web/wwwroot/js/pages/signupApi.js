4f4679b0f24e0a6003e539d6ed262f1dbb1cc18d
anudeepthi166 <anudeepthikolagani.999@gmail.com>
2025-01-24 10:14:42 +0530

    Fixed future date related issue

--- CaseManagement.Web/CaseManagement.Web/wwwroot/js/pages/signupApi.js
+++ CaseManagement.Web/CaseManagement.Web/wwwroot/js/pages/signupApi.js
@@ -3,14 +3,20 @@
         event.preventDefault();
         if ($(this).valid()) {
             console.log("Valid form, calling the API")
-
+            const dob = new Date($('#DateOfBirth').val());
+            const today = new Date();
+            if (dob > today) {
+                $('#DateOfBirth').addClass('is-invalid'); // Add error class
+                return alert("Date of birth should not be in future");
+            }
+            $('#DateOfBirth').removeClass('is-invalid'); // Add error class
             // Serialize form data
             const formDataArray = $(this).serializeArray();
             var formData = {}
             formDataArray.forEach(item => formData[item.name] = item.value)
             if (!formData.DateOfBirth) {
                 formData.DateOfBirth = null;
-            }
+            }          
             console.log("formData --", formData)
             $.ajax({
                 url: apiBaseUrl + "/SignUp/person",
