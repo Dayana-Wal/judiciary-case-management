c8d7f7b8d5e69035883db370a4569897431152d2
Dayana-Wal <dayana.b@veltris.com>
2025-02-04 14:03:53 +0530

    UI for viewing the case

---
+++ CaseManagement.Web/CaseManagement.Web/Controllers/ViewCaseController.cs
@@ -0,0 +1,26 @@
+﻿using CaseManagement.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseManagement.Web.Controllers
+{
+    [Route("[controller]")]
+    public class ViewCaseController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ViewCaseController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("CaseDetails")]
+        public IActionResult CaseView(string caseId)
+        {
+            var viewCaseObject = new ViewCaseModel();
+            var apiBaseUrl = _configuration["ApiSettings:BaseUrl"];
+            ViewBag.ApiBaseUrl = apiBaseUrl;
+            ViewBag.CaseId = caseId;
+            return View(viewCaseObject);
+        }
+    }
+}
